21d6313b757b5e50d67b5e44ef15f608e3f29400
Gaurav Sharma <gauravs08@users.noreply.github.com>
2019-03-16 20:02:20 +0200

    Stream creation Terminal operation on Stream Spliterator

---
+++ src/com/Java8/features/Stream/CreateVariousStream.java
@@ -0,0 +1,74 @@
+package com.Java8.features.Stream;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+
+/*
+ * https://www.journaldev.com/2774/java-8-stream
+ */
+public class CreateVariousStream {
+
+	public static void main(String[] args) {
+		//--1 | Creating various Streams
+		Stream<Integer> stream = Stream.of(1,2,3,4);
+		Stream<Integer> stre = Stream.of(new Integer[] {1,2,3,1,4});
+		
+		IntStream intStream = IntStream.of(1,2,3,4,5);
+		IntStream is2 = "abc".chars();
+		
+		LongStream is = Arrays.stream(new long[]{1,2,3,4});
+		
+		//Stream<Integer> stream1 = Stream.of(new int[]{1,2,3,4});  //// it doesn’t support autoboxing Compile time error, Type mismatch: cannot convert from Stream<int[]> to Stream<Integer>
+		
+		//--2 | Creating Stream through generate() and iterate
+		Stream<String> stream1 = Stream.generate(() -> {return "abc";});
+		Stream<String> stream2 = Stream.iterate("abc", (i) -> i);
+
+		
+		//--3 | Sequential & Parallel Stream creation
+		List<Integer> myList = new ArrayList<>();
+		for(int i=0; i<100; i++) myList.add(i);
+				
+		//sequential stream
+		Stream<Integer> sequentialStream = myList.stream();
+				
+		//parallel stream
+		Stream<Integer> parallelStream = myList.parallelStream();
+	
+	
+		//--4 | Converting Java Stream to Collection or Array
+		System.out.println("--Converting Java Stream to Collection or Array--");
+		Stream<Integer> intStream1 = Stream.of(1,2,3,4);
+		List<Integer> intList = intStream1.collect(Collectors.toList());  // converting Stream to List
+
+		System.out.println(intList); //prints [1, 2, 3, 4]
+
+		intStream1 = Stream.of(1,2,3,4); //stream is closed, so we need to create it again
+		Map<Integer,Integer> intMap = intStream1.collect(Collectors.toMap(i -> i, i -> i+10));  //Converting Stream to Map
+		System.out.println(intMap); //prints {1=11, 2=12, 3=13, 4=14}
+		
+		intStream1 = Stream.of(1,2,3,4);
+		ConcurrentMap<Object, Object> cMap = intStream1.collect(Collectors.toConcurrentMap(i ->i, i ->i+i)); //Converting into ConcurrentHashMap
+		
+		cMap.forEach((k,v)->System.out.println(k +"="+v));
+		
+		//--5 | Convert Stream to Array using .toArray
+		System.out.println("--Convert Stream to Array using .toArray----");
+		intStream1 = Stream.of(1,2,3,4);
+		Integer[] intArray = intStream1.toArray(Integer[]::new);
+		System.out.println(Arrays.toString(intArray)); //prints [1, 2, 3, 4]
+		
+		
+		
+	}
+
+}
---
+++ src/com/Java8/features/Stream/StreamMapFilterSortFlatMap.java
@@ -0,0 +1,71 @@
+package com.Java8.features.Stream;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamMapFilterSortFlatMap {
+
+	public static void main(String[] args) {
+
+		List<Integer> myList = new ArrayList<>();
+		for (int i = 0; i < 100; i++)
+			myList.add(i);
+		
+		Stream<Integer> sequentialStream = myList.stream();
+
+		Stream<Integer> highNums = sequentialStream
+				.filter(p -> p > 90); // filter numbers greater than 90
+		
+		System.out.println("High Nums greater than 90 =");
+		
+		highNums.forEach(p -> System.out.print(p + " "));
+		
+
+		
+		//2 | 
+		System.out.println("\n-- Stream .map --");
+		
+		Stream<String> names = Stream.of("aBc", "d", "ef");
+		
+		List<String> lt = (List<String>) names
+				.map(s -> s.toUpperCase())
+				.collect(Collectors.toList());
+		
+		System.out.println(lt);
+		
+		// 3 |
+		System.out.println("-- Sorting --");
+		Stream<String> names2 = Stream.of("aBc", "d", "ef", "123456");
+		
+		List<String> reverseSorted = names2
+				.sorted(Comparator.reverseOrder())
+				.collect(Collectors.toList());
+		
+		System.out.println(reverseSorted); // [ef, d, aBc, 123456]
+
+		Stream<String> names3 = Stream.of("aBc", "d", "ef", "123456");
+		
+		List<String> naturalSorted = names3
+				.sorted()
+				.collect(Collectors.toList());
+		
+		System.out.println(naturalSorted); //[123456, aBc, d, ef]
+	// 4 | // To iterate over Stream of Stream
+		System.out.println("--flatMap--");
+		Stream<List<String>> namesOriginalList = Stream.of(
+				Arrays.asList("Pankaj","Soni"), 
+				Arrays.asList("David", "Lisa"),
+				Arrays.asList("Amit","Roy"));
+			//flat the stream from List<String> to String stream
+			
+		Stream<String> flatStream = namesOriginalList
+				.flatMap(strList -> strList.stream());
+
+			flatStream.forEach(System.out::println);
+	}
+
+}
